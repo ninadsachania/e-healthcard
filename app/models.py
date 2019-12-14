d75c59c07ca83c159a739b5c5a34df19ff2f5dcf
Ninad Sachania <ninad.sachania@gmail.com>
2019-12-14 11:49:58 +0530

    Fix mistake

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
-    phone_number = db.Column(db.Integer(10), index=True, unique=True)
+    phone_number = db.Column(db.Integer, index=True, unique=True)
     email = db.Column(db.String(128), index=True, unique=True)
     password_hash = db.Column(db.String(128))
 
