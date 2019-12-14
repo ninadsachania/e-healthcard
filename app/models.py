71b41733a8ae713698d255046b5fca4fc43720d5
Ninad Sachania <ninad.sachania@gmail.com>
2019-12-14 12:25:58 +0530

    Implement login functionality

--- app/models.py
+++ app/models.py
@@ -1,10 +1,15 @@
-from app import db
+from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
-    phone_number = db.Column(db.Integer, index=True, unique=True)
+    phone_number = db.Column(db.String(10), index=True, unique=True)
     email = db.Column(db.String(128), index=True, unique=True)
     password_hash = db.Column(db.String(128))
 
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm, RegistrationForm
+from app.models import User
+from flask_login import current_user, login_user
 
 @app.route('/index')
 @app.route('/')
@@ -9,16 +11,24 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     
     if form.validate_on_submit():
-        flash('Login required for user: {}, remember_me: {}'.format(form.username.data, form.remember_me.data))
+        user = User.query.filter_by(email=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid email or password!')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
 
     return render_template('login.html', form=form, title="Login")
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = RegistrationForm()
 
     if form.validate_on_submit():
