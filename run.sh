e5bfac1c2cd964180f63257b9a4a6aefdb317ed5
Ninad Sachania <ninad.sachania@gmail.com>
2019-12-21 21:09:19 +0530

    Add ability to send errors by email when not in debug mode

--- run.sh
+++ run.sh
@@ -2,4 +2,11 @@
 
 export FLASK_APP=main.py
 export PYTHONDONTWRITEBYTECODE=1
+
+export MAIL_SERVER='smtp.gmail.com'
+export MAIL_PORT=587
+export MAIL_USE_TLS=1
+# export MAIL_USERNAME=''
+# export MAIL_PASSWORD=''
+
 ./main.py
