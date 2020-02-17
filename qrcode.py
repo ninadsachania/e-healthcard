a9b38bbde2819962311f25d06fb52a72bf83e57a
Ninad Sachania <ninad.sachania@gmail.com>
2020-02-17 19:24:15 +0530

    Add script to make qrcodes containing patient's information.

---
+++ qrcode.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python3
+
+import os
+import json
+import segno
+
+
+def make_qrcode(information: {}):
+    qrcode = segno.make(str(json.dumps(information)), micro=False)
+    path = os.path.join(
+        os.path.abspath(os.getcwd()),
+        'app',
+        'static',
+        'qrcodes',
+        '{}.png'.format(information['id'])
+    )
+
+    qrcode.save(path, scale=5)
+
+
+def qrcode_path(information: {}):
+    make_qrcode(information)
+    path = '/static/qrcodes/{}.png'.format(information['id'])
+    return path
