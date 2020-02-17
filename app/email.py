dec445856143605e017e13740734611f5ddacff9
Ninad Sachania <ninad.sachania@gmail.com>
2020-02-17 19:19:36 +0530

    pep8

--- app/email.py
+++ app/email.py
@@ -2,11 +2,13 @@ from flask_mail import Message
 from app import mail, app
 from flask import render_template
 
+
 def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
     mail.send(msg)
+
 
 def send_password_reset_email(user):
     token = user.get_reset_password_token()
