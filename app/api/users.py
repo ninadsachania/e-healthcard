444adb866fceafe5b97404dd2aa5c98ed36d32b5
Ninad Sachania <ninad.sachania@gmail.com>
2020-03-14 23:55:39 +0530

    Change routes and remove user id from URLs

--- app/api/users.py
+++ app/api/users.py
@@ -1,14 +1,15 @@
 from app.api import bp
-from flask import request, jsonify
-from app.models import User, Metadata
+from flask import request, jsonify, g
+from app.models import User, Metadata, StaticInformation, DynamicInformation
 from app import db
 from app.api.errors import bad_request, error_response
+from app.api.auth import token_auth
 
 
-@bp.route('/users/<int:id>', methods=['GET'])
-def get_user(id):
-    user = User.query.filter_by(id=id).first()
-
+@bp.route('/users', methods=['GET'])
+@token_auth.login_required
+def get_user():
+    user = g.current_user
     if user:
         return jsonify(user.to_dict())
 
@@ -57,12 +58,45 @@ def create_user():
     return jsonify(data)
 
 
-@bp.route('/users/', methods=['GET'])
+@bp.route('/users/all', methods=['GET'])
 def users():
     users = User.query.all()
     data = []
 
     for user in users:
         data.append(user.to_dict())
+
+    return jsonify(data)
+
+
+@bp.route('/users/static_information')
+@token_auth.login_required
+def static_information():
+    user = g.current_user
+
+    if user is None:
+        return error_response(404, "User not found")
+
+    info = StaticInformation.query.filter_by(user_id=user.id).first()
+
+    if info is None:
+        return error_response(204, message="No data found")
+
+    return jsonify(info.to_dict())
+
+
+@bp.route('/users/dynamic_information')
+@token_auth.login_required
+def dynamic_information():
+    user = g.current_user
+
+    records = DynamicInformation.query.filter_by(user_id=user.id).first()
+    if records is None:
+        return error_response(204, "No data found")
+
+    data = []
+
+    for record in records:
+        data.append(record.to_dict())
 
     return jsonify(data)
