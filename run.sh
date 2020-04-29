d5e2e4235675c78ee5c308d4c85de24016ce032f
Ninad Sachania <ninad.sachania@gmail.com>
2020-04-29 22:40:42 +0530

    Add mail username and password variables

--- run.sh
+++ run.sh
@@ -6,8 +6,8 @@ export PYTHONDONTWRITEBYTECODE=1
 export MAIL_SERVER='smtp.gmail.com'
 export MAIL_PORT=587
 export MAIL_USE_TLS=1
-# export MAIL_USERNAME=''
-# export MAIL_PASSWORD=''
+export MAIL_USERNAME=pbtemp.xyz@gmail.com
+export MAIL_PASSWORD=PBtemp@123
 
 chmod u+x ./main.py
 ./main.py
